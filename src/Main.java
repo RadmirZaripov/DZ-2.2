a52eecc2dfd9151e6a7a732f345e094a4e98f5f1
unknown <kenseisan23@gmail.com>
2023-04-13 01:17:34 +0300

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100;
-        int replenishment = 11git00;
+        int replenishment = 11;
 
         if (replenishment > 100) {
             int bonus = replenishment / 100;
