f0fb699b434ce81c135c3bba48d546cec202cad2
unknown <kenseisan23@gmail.com>
2023-04-13 01:15:32 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int check = 100;
+        int replenishment = 11git00;
+
+        if (replenishment > 100) {
+            int bonus = replenishment / 100;
+            System.out.println("Количество бонусов:" + bonus);
+            System.out.println("Итоговый счет:" + (check + replenishment + bonus));
+        } else {
+            int bonus = 0;
+            System.out.println("Количество бонусов:" + bonus);
+            System.out.println("Итоговый счет:" + (check + replenishment));
+        }
+
+    }
+
+}
